d5cce88a5b56d1099f0626d49db468df5c17a04a
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-16 10:03:14 +0900

    EmployeeService update

--- src/main/java/com/employee/api/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/employee/api/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,15 @@
 package com.employee.api.service.impl;
 
 import com.employee.api.dto.EmployeeDto;
+import com.employee.api.entity.Department;
+import com.employee.api.entity.Employee;
+import com.employee.api.exception.ResourceNotFoundException;
+import com.employee.api.mapper.EmployeeMapper;
 import com.employee.api.repository.DepartmentRepository;
 import com.employee.api.repository.EmployeeRepository;
 import com.employee.api.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +20,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
-
     private final DepartmentRepository departmentRepository;
 
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return null;
+        //입력 받은 DTO => Entity
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        //Department 의 존재여부를 조회
+        Department department = departmentRepository.findById(employeeDto.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department is not exists with id: " +
+                                employeeDto.getDepartmentId(),
+                                HttpStatus.NOT_FOUND));
+        //Employee 와 Department 연결
+        employee.setDepartment(department);
+        //Employee 등록
+        Employee savedEmployee = employeeRepository.save(employee);
+        //DB에 등록된 Entity => DTO
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 
     @Override
